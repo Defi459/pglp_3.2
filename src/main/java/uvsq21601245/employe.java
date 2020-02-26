11a794aeab9545777eee44831ac1c759a85f5061
Edouard Longueville <edouard.longueville@ens.uvsq.fr>
2020-02-26 10:51:30 +0100

    employe fait

--- src/main/java/uvsq21601245/employe.java
+++ src/main/java/uvsq21601245/employe.java
@@ -1,4 +1,31 @@
 package uvsq21601245;
 
 public class employe {
+
+    private int salaire;
+    private int anciennete;
+
+    employe(int anciennete){
+
+        this.anciennete=anciennete;
+        this.salaire=1500+20*anciennete;
+
+    }
+
+    void nouvelle_annee(){
+
+        anciennete++;
+        this.salaire=1500+20*anciennete;
+
+    }
+
+    int get_salaire(){
+        return this.salaire;
+    }
+
+    int get_anciennete(){
+        return this.anciennete;
+    }
+
+
 }
--- src/test/java/uvsq21601245/employetest.java
+++ src/test/java/uvsq21601245/employetest.java
@@ -1,4 +1,28 @@
 package uvsq21601245;
 
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class employetest {
+
+    @Test
+    public void testSalaire(){
+        employe e = new employe(0);
+        assertEquals(e.get_salaire(),1500);
+    }
+
+    @Test
+    public void testAnciennete(){
+        employe e =new employe(5);
+        assertEquals(e.get_salaire(),1600);
+    }
+
+    @Test
+    public void testNouvelleAnnee(){
+        employe e = new employe(0);
+        e.nouvelle_annee();
+        assertEquals(e.get_salaire(),1520);
+    }
 }
